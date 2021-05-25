10141b64dba7220319f843c7ab5689a6394aca09
James S. Moore <james.moore@johnstonhowse.com>
2021-05-25 00:23:00 -0500

    FI-172 notes and changes to test prod

--- TestCluster/main.tf
+++ TestCluster/main.tf
@@ -1,13 +1,17 @@
+# TODO: abstract variables - not allowed in this block
 terraform {
   backend "remote" {
-    organization = "example-org-186bcc"
+    #organization = "example-org-186bcc"
+    organization = "JohnstonHowse"
 
     workspaces {
-      name = "jh-test-cli"
+      #name = "jh-test-cli"
+      name = "cli-vsphere-testing"
     }
   }
 }
 
+# TODO: Implement local copy of module
 /*
 module "testcluster1" {
   source = "../modules/terraform-vsphere-vm"
@@ -25,6 +29,7 @@ provider "vsphere" {
   allow_unverified_ssl = true
   client_debug = true
 }
+
 data "vsphere_datacenter" "dc" {
   name = var.vsphere_datacenter
 }
@@ -32,6 +37,7 @@ data "vsphere_datastore" "datastore" {
   name          = var.vsphere_datastore
   datacenter_id = data.vsphere_datacenter.dc.id
 }
+/*
 data "vsphere_resource_pool" "pool" {
   name          = var.vsphere_resource_pool
   datacenter_id = data.vsphere_datacenter.dc.id
@@ -70,3 +76,4 @@ resource "vsphere_virtual_machine" "vm2" {
     size  = 20
   }
 }
+*/
