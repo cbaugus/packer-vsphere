f1af8c28b59ecac057ec66a24b224170ae66d900
James S. Moore <james.moore@johnstonhowse.com>
2021-05-25 16:41:16 -0500

    FI-223 implementing VM template

--- TestCluster/main.tf
+++ TestCluster/main.tf
@@ -50,13 +50,18 @@ data "vsphere_network" "network" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+data "vsphere_virtual_machine" "template" {
+  name          = "linux-server-ubuntu-20-lts-server-jsm"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-base1"
   resource_pool_id = data.vsphere_compute_cluster.compute_cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
   num_cpus = 2
   memory   = 1024
-  guest_id = "other3xLinux64Guest"
+  guest_id = data.vsphere_virtual_machine.template.guest_id
   network_interface {
     network_id = data.vsphere_network.network.id
   }
