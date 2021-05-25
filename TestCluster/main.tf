d1aabb9c73cc7a6352ea4d64c04abd1c6c5be1bf
James S. Moore <james.moore@johnstonhowse.com>
2021-05-24 23:19:53 -0500

    FI-172 implemented secrets.auto, README and refactored main.tf to match

--- TestCluster/main.tf
+++ TestCluster/main.tf
@@ -1,16 +1,72 @@
 terraform {
   backend "remote" {
-    organization = "JohnstonHowse"
+    organization = "example-org-186bcc"
 
     workspaces {
-      name = "cli-vsphere-testing"
+      name = "jh-test-cli"
     }
   }
 }
 
+/*
 module "testcluster1" {
   source = "../modules/terraform-vsphere-vm"
   dc = ""
   vmrp = ""
   vmtemp = ""
+}
+*/
+
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_pass
+  vsphere_server = var.vsphere_server
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+  client_debug = true
+}
+data "vsphere_datacenter" "dc" {
+  name = var.vsphere_datacenter
+}
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+data "vsphere_resource_pool" "pool" {
+  name          = var.vsphere_resource_pool
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+data "vsphere_network" "network" {
+  name          = var.vsphere_network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-base1"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
+resource "vsphere_virtual_machine" "vm2" {
+  name             = "terraform-base2"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+  disk {
+    label = "disk0"
+    size  = 20
+  }
 }
---
+++ TestCluster/variables.tf
@@ -0,0 +1,31 @@
+variable "vsphere_user" {
+  description = "vSphere administrator username"
+  type        = string
+  sensitive   = true
+}
+variable "vsphere_pass" {
+  description = "vSphere administrator password"
+  type        = string
+  sensitive   = true
+}
+variable "vsphere_server" {
+  description = "vSphere server address"
+  type        = string
+  sensitive   = true
+}
+variable "vsphere_datacenter" {
+  description = "vSphere datacenter"
+  type        = string
+}
+variable "vsphere_datastore" {
+  description = "vSphere datastore"
+  type        = string
+}
+variable "vsphere_resource_pool" {
+  description = "vSphere resource pool"
+  type        = string
+}
+variable "vsphere_network" {
+  description = "vSphere network"
+  type        = string
+}
