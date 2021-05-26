0998d9e785c7a45d6258b560d28e4e711e2fa5ae
James S. Moore <james.moore@johnstonhowse.com>
2021-05-26 08:46:49 -0500

    FI-223 abstract data, provider and resources

---
+++ TestCluster/data.tf
@@ -0,0 +1,23 @@
+data "vsphere_datacenter" "dc" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_compute_cluster" "compute_cluster" {
+  name          = "dal-w01-cl01"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vsphere_network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "linux-server-ubuntu-20-lts-server-jsm"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
---
+++ TestCluster/resources.tf
@@ -0,0 +1,15 @@
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-base1"
+  resource_pool_id = data.vsphere_compute_cluster.compute_cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus = 2
+  memory   = 1024
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
