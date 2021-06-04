86a9b16b7a77aa47818d075f4350e1818d55516a
Dustin Grider <dustin.grider@johnstonhowse.com>
2021-06-04 15:45:01 -0400

    [FI-173] vm module implementation work   - ran terraform fmt   - a side by side with module cluster   - added example with var breakout

---
+++ TestCluster/backend.tf
@@ -0,0 +1,12 @@
+# TODO: abstract variables - not allowed in this block
+# https://www.terraform.io/docs/language/settings/backends/remote.html#prefix
+terraform {
+  backend "remote" {
+
+    organization = "JohnstonHowse"
+
+    workspaces {
+      name = "cli-vsphere-testing2"
+    }
+  }
+}
