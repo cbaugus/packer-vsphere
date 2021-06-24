bfb16b7af8ecac1f24057ce68ea7c5ba960d6691
James S. Moore <james.moore@johnstonhowse.com>
2021-06-23 21:37:39 -0500

    FI-380 configure for multiple vms with different OS type

--- AnsibleDeploymentTesting/datasources.tf
+++ AnsibleDeploymentTesting/datasources.tf
@@ -22,8 +22,21 @@ data "vsphere_network" "network" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_virtual_machine" "template" {
-  name          = var.vsphere_template
+data "vsphere_virtual_machine" "template_ubuntu" {
+  name          = var.vsphere_template_ubuntu
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+data "vsphere_virtual_machine" "template_debian" {
+  name          = var.vsphere_template_debian
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template_centos" {
+  name          = var.vsphere_template_centos
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+data "vsphere_virtual_machine" "template_photon" {
+  name          = var.vsphere_template_photon
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
--- AnsibleDeploymentTesting/variables.tf
+++ AnsibleDeploymentTesting/variables.tf
@@ -41,6 +41,22 @@ variable "vsphere_template" {
   description = "vSphere template for creating VM"
   type        = string
 }
+variable "vsphere_template_ubuntu" {
+  description = "vSphere template for creating VM (Ubuntu)"
+  type        = string
+}
+variable "vsphere_template_debian" {
+  description = "vSphere template for creating VM (Debian)"
+  type        = string
+}
+variable "vsphere_template_centos" {
+  description = "vSphere template for creating VM (CentOS)"
+  type        = string
+}
+variable "vsphere_template_photon" {
+  description = "vSphere template for creating VM (Photon OS)"
+  type        = string
+}
 variable "name_prefix" {
   description = "Prefix for naming convention of VMs"
   type        = string
