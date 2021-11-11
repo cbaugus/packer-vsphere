3e3c8781ac8c99954d54f936dd152f18b5c4b613
Chris Baugus <cbaugus@me.com>
2021-11-11 13:34:36 -0600

    disk build testing

--- coa-cluster/locals.tf
+++ coa-cluster/locals.tf
@@ -15,17 +15,19 @@ locals {
 
   s3_provisioned_disks = [
     {
-      S3_HOST_1                 = "https://devtest.freenas.tmi.jhdc.local:9000"
-      S3_MOUNT_1                = "/mnt/local/template_mount"
-      S3_UID_1                  = "1000"
-      S3_GID_1                  = "1004"
-      S3_ACL_1                  = "private"
-      S3_CACHE_1                = "/mnt/local/template_cache"
-      S3_BUCKET_1               = "template-bucket"
-      S3_ACCESS_KEY_1           = data.vault_generic_secret.minio_s3.data["access_key"]
-      S3_SECRET_KEY_1           = data.vault_generic_secret.minio_s3.data["secret_key"]
-      S3_NO_CHECK_CERTIFICATE_1 = "true"
-      S3_SSL_VERIFY_HOSTNAME_1  = "0"
+      DEVICE_DRIVE = "sdb"
+      LABEL = "elastic"
+      S3_HOST_1                 = ""
+      S3_MOUNT_1                = ""
+      S3_UID_1                  = ""
+      S3_GID_1                  = ""
+      S3_ACL_1                  = ""
+      S3_CACHE_1                = ""
+      S3_BUCKET_1               = ""
+      S3_ACCESS_KEY_1           = ""
+      S3_SECRET_KEY_1           = ""
+      S3_NO_CHECK_CERTIFICATE_1 = ""
+      S3_SSL_VERIFY_HOSTNAME_1  = ""
     }
   ]
 }
