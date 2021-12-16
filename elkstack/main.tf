9adecd86104cc774128f27becb9188a25e5c6ae5
Chris Baugus <cbaugus@me.com>
2021-12-15 18:03:32 -0600

    update consul overrides

--- elkstack/main.tf
+++ elkstack/main.tf
@@ -31,10 +31,11 @@ module "cluster" {
   growr_provisioned_disks = local.growr_provisioned_disks
 
   #Consul overrides
-  consul_acl_token      = var.consul_acl_token
-  consul_pass    = var.consul_pass
-  consul_raw_key = var.consul_raw_key
-  consul_iptables_enable = "false"
+  consul_pass            = var.consul_pass
+  consul_raw_key         = var.consul_raw_key
+  consul_addresses_http  = "{{ consul_bind_address }} 127.0.0.1"
+  consul_ports           = { "grpc" = "8502", "dns" = "8600", "http" = "8500", "https" = "-1", "rpc" = "8400", "serf_lan" = "8301", "serf_wan" = "8302", "server" = "8300" }
+  consul_acl_agent_token = var.consul_acl_token
 
   #Nomad overrides
   nomad_region                = var.nomad_region
@@ -44,7 +45,7 @@ module "cluster" {
   nomad_options               = var.nomad_options
   nomad_meta                  = var.nomad_meta
   nomad_host_volumes          = var.nomad_host_volumes
-  nomad_consul_token          = var.nomad_consul_acl_token
+  nomad_consul_token          = var.nomad_consul_token
 
   #Vault overrides
   vault_agent_role_id            = var.vault_agent_role_id
