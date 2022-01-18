d8128bd59932df978257b6b26ef8c10963dfe87a
stevenheinze57 <steven.heinze@johnstonhowse.com>
2022-01-18 14:07:02 -0500

    Merge branch 'com6' into develop
    # Conflicts:
    #	nomad-cluster/main.tf

--- nomad-cluster/main.tf
+++ nomad-cluster/main.tf
@@ -26,23 +26,23 @@ module "vault_cluster" {
   vsphere_tag_ids         = [data.vsphere_tag.nomad.id]
 
   #Terraform Provisioner required inputs
-  path_to_ansible          = "../../ansible-deployments/vault-server.yml"
+  path_to_ansible          = "../../ansible-deployments/nomad-server.yml"
   remote_exec_ssh_key_file = var.remote_exec_ssh_key_file
   local_exec_ssh_key_file  = var.local_exec_ssh_key_file
   local_exec_user          = var.local_exec_user
 
-  nomad_node_role = "server"
-  nomad_region    = var.nomad_region
-  nomad_datacenter = var.vsphere_datacenter //this ties the name of the Nomad DC to the vSphere DC name
-  nomad_authoritative_region = "tmi" //this is not a variable because all Nomad clusters should refer to TMI as the single source of ACLs
+  nomad_node_role            = "server"
+  nomad_region               = var.nomad_region
+  nomad_datacenter           = var.vsphere_datacenter //this ties the name of the Nomad DC to the vSphere DC name
+  nomad_authoritative_region = "tmi"                  //this is not a variable because all Nomad clusters should refer to TMI as the single source of ACLs
   //nomad_network_interface = "" likely want this to be ens192
   nomad_meta = {
     "node-switcher" = "on"
     "purpose"       = "nomad"
   }
   // nomad_docker_enable = "no" the default is yes for the cluster module - not sure what we want on servers yet
-  nomad_use_consul = "no" //default is yes, need to flip this back on once Consul client provisioning is configured, will need other nomad_consul vars to go along with this
-  nomad_acl_enabled = ""
+  nomad_use_consul            = "no" //default is yes, need to flip this back on once Consul client provisioning is configured, will need other nomad_consul vars to go along with this
+  nomad_acl_enabled           = ""
   nomad_acl_replication_token = ""
-  nomad_vault_enabled = "no" //must change back to yes once Vault is ready
+  nomad_vault_enabled         = "no" //must change back to yes once Vault is ready
 }
