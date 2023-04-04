34f3aee4cdcfb424a5e044bc30100d96647c85b2
Chris Baugus <cbaugus@me.com>
2023-04-04 12:09:31 -0500

    Waypoint Worker in Prod-B

--- waypoint-v2/main.tf
+++ waypoint-v2/main.tf
@@ -1,6 +1,6 @@
 module "cluster" {
   source  = "app.terraform.io/baugus-lab/cluster-module/vsphere"
-  version = "2.1.3"
+  version = "2.1.4"
 
   #Cluster vars
   num_instances      = var.num_instances
