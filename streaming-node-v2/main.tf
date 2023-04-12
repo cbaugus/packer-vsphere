04b61bde0be082263fe048cb5e8466fb0c9bedd1
Chris Baugus <cbaugus@me.com>
2023-04-12 17:27:21 -0500

    update module var

--- streaming-node-v2/main.tf
+++ streaming-node-v2/main.tf
@@ -1,6 +1,6 @@
 module "cluster" {
   source  = "app.terraform.io/baugus-lab/cluster-module/vsphere"
-  version = "2.1.8"
+  version = "2.1.9"
 
   #Cluster vars
   num_instances      = var.num_instances
