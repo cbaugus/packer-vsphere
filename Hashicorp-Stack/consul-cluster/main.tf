59eac273d28c6967898716e9074e12adbf9c73f5
Chris Baugus <cbaugus@me.com>
2023-06-20 16:14:34 -0500

    consul zone a

--- Hashicorp-Stack/consul-cluster/main.tf
+++ Hashicorp-Stack/consul-cluster/main.tf
@@ -1,6 +1,6 @@
 module "cluster" {
   source  = "app.terraform.io/baugus-lab/cluster-module/vsphere"
-  version = "2.1.2"
+  version = "2.1.12"
 
   #Cluster vars
   num_instances    = var.num_instances
@@ -36,7 +36,7 @@ module "cluster" {
   consul_raw_key = var.consul_raw_key
   consul_iptables_enable = "false"
   consul_dnsmasq_enable = "True"
-  consul_dnsmasq_servers = [ "10.254.203.11",  "10.254.203.12" ]
+  consul_dnsmasq_servers = [ "" ]
   consul_dnsmasq_revservers = [ "10.254.0.0/16" ]
 
   #Nomad overrides
