70cd3572a7104f3366285b7a49801e9492229669
Chris Baugus <cbaugus@me.com>
2023-06-26 23:08:42 -0500

    start building nodes for Postgis and PostGRE Databases

--- database/postgre-fec-node-v2/main.tf
+++ database/postgre-fec-node-v2/main.tf
@@ -21,7 +21,7 @@ module "cluster" {
   vsphere_pass            = var.vsphere_pass
 
   #Terraform Provisioner required inputs
-  path_to_ansible          = "../../../ansible-deployments/host-volume-node-bootstrap.yml.yml"
+  path_to_ansible          = "../../../ansible-deployments/host-volume-node-bootstrap.yml"
   remote_exec_ssh_key_file = var.remote_exec_ssh_key_file
   local_exec_ssh_key_file  = var.local_exec_ssh_key_file
   local_exec_user          = var.local_exec_user
